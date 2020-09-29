40017cff1d5cf7dae2d845c215a1abd7a4349b5e
George Jones <georgejones88@Georges-Air.lan1>
2020-09-29 09:23:28 -0400

    python hw

--- house_hunting.py
+++ house_hunting.py
@@ -1,1 +1,17 @@
 # Write your code here
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+total_cost = input("what is the cost of your dream home?")
+print(total_cost)
+portion_down_payment = float(total_cost) * .25
+print(portion_down_payment)
+r = .04
+annual_salary = input("how much do you make annually?")
+print(annual_salary)
+percent_saved = input("enter the percent of salary to save, as a decimal")
+portion_saved = float(annual_salary) * float(percent_saved)
+print(portion_saved)
+# current_savings = 
